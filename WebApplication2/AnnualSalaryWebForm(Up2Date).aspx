0edf72473a091c2555c941eb240d4305a9695293
Damir Wedderburn <damir.wedderburn@gmail.com>
2017-01-22 10:37:44 -0500

    Add project files.

---
+++ WebApplication2/AnnualSalaryWebForm(Up2Date).aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AnnualSalaryWebForm(Up2Date).aspx.cs" Inherits="WebApplication2.AnnualSalaryWebForm_Up2Date_" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Wage:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    
+    </div>
+        <p>
+            Weekly Hours:
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Estimate:
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem>AnnualSalary</asp:ListItem>
+                <asp:ListItem>MonthlyWages</asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calculate" />
+    </form>
+</body>
+</html>
---
+++ WebApplication2/AnnualSalaryWebForm(Up2Date).aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class AnnualSalaryWebForm_Up2Date_ : System.Web.UI.Page
+    {
+        public object C { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference2.WebService1SoapClient s = new ServiceReference2.WebService1SoapClient();
+
+            double answer = 0.0;
+            String select = DropDownList1.SelectedValue;
+
+
+            if (select.Equals("AnnualSalary"))
+            {
+                answer = s.AnnualSalary(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text));
+
+
+            }
+            else if (select.Equals("MonthlyWages"))
+            {
+                answer = s.MonthlyWages(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text));
+
+
+            }
+            TextBox3.Text = string.Format("{0:C}", answer);
+
+
+
+
+
+        }
+    }
+}
